26ff864362dd383e8f1a29d42e08e2d1a9e70f14
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2016-10-25 10:27:17 -0500

    Moved bot to new file

--- bot.py
+++
@@ -1,9 +0,0 @@
-from chatterbot import ChatBot
-
-chatbot = ChatBot(
-    'Ron Obvious',
-    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
-)
-
-chatbot.train("chatterbot.corpus.english")
-print(chatbot.get_response(""))
