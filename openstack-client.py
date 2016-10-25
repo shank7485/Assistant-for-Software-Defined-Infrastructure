e109c913dbf6ecfd371c0571d8fc284262c5f629
Shashank Kumar Shankar <shashank.kumar.shankar@intel.com>
2016-10-25 12:03:10 -0500

    Moved Bot to base

--- openstack-client.py
+++ openstack-client.py
@@ -1,1 +1,16 @@
-# Todo
+from novaclient import client
+
+VERSION = '2'
+USERNAME = 'admin'
+PASSWORD = '1'
+PROJECT_ID = 'cd1e4f37af1e4b58bcf8c4b6c18c3c5a'
+AUTH_URL = 'http://172.99.106.120:5000/'
+
+
+nova = client.Client(VERSION, USERNAME, PASSWORD, PROJECT_ID, AUTH_URL)
+print(nova.flavors.list())
+
+#
+# from subprocess import call
+#
+# call(["ls", "-l"])
